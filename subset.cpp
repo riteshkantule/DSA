84c436bdcabc981200d803934b0506b14e21c13c
Ritesh <106819295+riteshkantule@users.noreply.github.com>
2023-05-17 19:17:56 +0530

    Add files via upload
    subset algorithm recursive

---
+++ subset.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void solve(string in, string out){
+    
+    if (in.length()==0){
+        cout<< out << " ";
+        return;
+    }
+    
+    string out1 = out;
+    string out2 = out;
+    
+    out2.push_back(in[0]);
+    in.erase(in.begin()+0);
+    
+    solve(in, out1);
+    solve(in, out2);
+    
+    return;
+    
+}
+
+int main()
+{
+    string in;
+    cin >> in;
+    
+    string out = " ";
+    
+    solve(in, out);
+
+    return 0;
+}
