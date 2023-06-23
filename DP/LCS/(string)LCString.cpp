e76e6fbf867933c18710ab3189992fe39a4edd72
Ritesh <106819295+riteshkantule@users.noreply.github.com>
2023-06-24 00:31:43 +0530

    Update (string)LCString.cpp

--- DP/LCS/(string)LCString.cpp
+++ DP/LCS/(string)LCString.cpp
@@ -1,66 +1,59 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
-int commom_substring(string str, string frr) {
-    int n = str.length();
-    int m = frr.length();
-    int t[n + 1][m + 1];
+string common_subsequence(string str1, string str2) {
+    int n = str1.length();
+    int m = str2.length();
+    vector<vector<int>> t(n + 1, vector<int>(m + 1));
 
-    for (int i = 0; i < n + 1; i++)
+    for (int i = 0; i <= n; ++i)
         t[i][0] = 0;
-    for (int j = 0; j < m + 1; j++)
+
+    for (int j = 0; j <= m; ++j)
         t[0][j] = 0;
 
-    for (int i = 1; i < n + 1; ++i) {
-        for (int j = 1; j < m + 1; ++j) {
-            if (str[i - 1] == frr[j - 1]) {
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= m; ++j) {
+            if (str1[i - 1] == str2[j - 1]) {
                 t[i][j] = 1 + t[i - 1][j - 1];
             } else {
-                t[i][j] = 0;
+                t[i][j] = max(t[i - 1][j], t[i][j - 1]);
             }
         }
-    }
-    
-    int ans = 0;
-    int iidx = -1;
-    int jidx = -1;
-    
-    for (int i = 0; i < n + 1; i++) {
-        for (int j = 0; j < m + 1; j++) {
-            if (t[i][j] > ans) {
-                ans = t[i][j];
-                iidx = i;
-                jidx = j;
-            }
-            cout << t[i][j] << " ";
-        }
-        cout << endl;
     }
 
-    cout << iidx << " " << jidx << endl;
-    string temp = "";
-    
-    while (iidx > 0 && jidx > 0) {
-        if (t[iidx][jidx] == 0) break;
-        temp += str[iidx - 1];
-        iidx--;
-        jidx--;
+    string ans = "";
+    int i = n;
+    int j = m;
+    while (i > 0 && j > 0) {
+        if (str1[i - 1] == str2[j - 1]) {
+            ans.push_back(str1[i - 1]);
+            i--;
+            j--;
+        } else {
+            if (t[i - 1][j] > t[i][j - 1]) {
+                i--;
+            } else {
+                j--;
+            }
+        }
     }
-    
-    reverse(temp.begin(), temp.end());
-    cout << temp << endl;
+
+    reverse(ans.begin(), ans.end());
     return ans;
 }
 
 int main() {
+    string str1;
+    string str2;
+    cin >> str1 >> str2;
     
-    string str;
-    string frr;
-    cin >> str >> frr;
-    cout << commom_substring(str, frr);
+    string result = common_subsequence(str1, str2);
+    cout << "Longest Common Subsequence: " << result << endl;
 
     return 0;
 }
