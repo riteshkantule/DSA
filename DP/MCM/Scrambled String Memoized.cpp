1d60d50333302d5e9c1a85e19db8134ba3d2dd77
Ritesh <106819295+riteshkantule@users.noreply.github.com>
2023-07-03 14:20:38 +0530

    Create Scrambled String Memoized.cpp
    Scramble String using Memoization(Dynamic Programming)
    Given a string s1, we may represent it as a binary tree by partitioning it to two non-empty substrings recursively.
    Below is one possible representation of A = “great”:
     great
       /    \
      gr    eat
     / \    /  \
    g   r  e   at
               / \
              a   t
    To scramble the string, we may choose any non-leaf node and swap its two children.
    
    For example, if we choose the node “gr” and swap its two children, it produces a scrambled string “rgeat”.
    
        rgeat
       /    \
      rg    eat
     / \    /  \
    r   g  e   at
               / \
              a   t
    We say that “rgeat” is a scrambled string of “great”.
    
    Similarly, if we continue to swap the children of nodes “eat” and “at”, it produces a scrambled string “rgtae”.
    
        rgtae
       /    \
      rg    tae
     / \    /  \
    r   g  ta  e
           / \
          t   a
    We say that “rgtae” is a scrambled string of “great”.

---
+++ DP/MCM/Scrambled String Memoized.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+unordered_map <string, bool> mp;
+// key str = "a"+' '+"b"
+
+bool solve (string a, string b){
+    
+    if (a.compare(b)==0){
+        return true;
+    }
+    if (a.length()<=1){
+        return false;
+    }
+    string key = a;
+    key.push_back(' ');
+    key.append(b);
+    
+    if (mp.find(key)!=mp.end()){
+        return mp[key];
+    }
+    
+    int n = a.length();
+    bool flag = false;
+    
+    for (int i = 1; i<=n-1; i++){
+        bool condition1 = ((solve(a.substr(0, i), b.substr(n-i, i))==true)&&(solve(a.substr(i, n-i), b.substr(0, n-i))==true));
+        bool condition2 = ((solve(a.substr(0, i), b.substr(0, i))==true)&&(solve(a.substr(i, n-i), b.substr(i, n-i))==true));
+        if (condition1 || condition2){
+            flag = true;
+            break;
+        }
+    }
+    return mp[key] = flag;
+}
+
+int main(){
+    
+	string a = "great";
+	string b = "rgeat";
+	
+	if (a.length()!=b.length()){
+        return false;
+    }
+    if (a.length()==0){
+        return true;
+    }
+	cout << solve(a, b);
+
+	return 0;
+}
