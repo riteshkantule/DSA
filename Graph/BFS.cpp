0ebf4d68bf678db2e4c9b714dbd9bf2e2e0eb333
Ritesh <106819295+riteshkantule@users.noreply.github.com>
2023-07-05 19:16:04 +0530

    Update BFS.cpp
    Output: 0 1 4 2 3
    
    Time Complexity: O(N) + O(2E), Where N = Nodes, 2E is for total degrees as we traverse all adjacent nodes.
    
    Space Complexity: O(3N) ~ O(N), Space for queue data structure visited array and an adjacency list

--- Graph/BFS.cpp
+++ Graph/BFS.cpp
@@ -2,58 +2,57 @@
 using namespace std;
 
 class Solution {
-  public:
-    // Function to return Breadth First Traversal of given graph.
-    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
-        int vis[V] = {0}; 
-        vis[0] = 1; 
-        queue<int> q;
-        // push the initial starting node 
-        q.push(0); 
-        vector<int> bfs; 
-        // iterate till the queue is empty 
-        while(!q.empty()) {
-           // get the topmost element in the queue 
-            int node = q.front(); 
-            q.pop(); 
-            bfs.push_back(node); 
-            // traverse for all its neighbours 
-            for(auto it : adj[node]) {
-                // if the neighbour has previously not been visited, 
-                // store in Q and mark as visited 
-                if(!vis[it]) {
-                    vis[it] = 1; 
-                    q.push(it); 
+    public:
+        vector <int> bsfOfGraph(int V, vector <int> adj[]) {
+            int vis[V] = {0};
+            vis[0] = 1;
+
+            queue <int> q;
+
+            q.push(0);
+            vector <int> bfs;
+
+            while (!q.empty()){
+                int node = q.front();
+                q.pop();
+                bfs.push_back(node);
+
+                for (auto it : adj[node]){
+                    if (!vis[it]){
+                        vis[it] = 1;
+                        q.push(it);
+
+                    }
                 }
             }
+            return bfs;
         }
-        return bfs; 
-    }
 };
 
-void addEdge(vector <int> adj[], int u, int v) {
+void addEdge(vector <int> adj[], int u, int v){
     adj[u].push_back(v);
     adj[v].push_back(u);
 }
 
-void printAns(vector <int> &ans) {
-    for (int i = 0; i < ans.size(); i++) {
-        cout << ans[i] << " ";
+void PrintBFS(vector <int> &ans){
+    for(int i=0; i<ans.size(); i++){
+        cout << ans[i]<< " ";
     }
 }
 
-int main() 
-{
+int main(){
+
     vector <int> adj[6];
-    
+
     addEdge(adj, 0, 1);
     addEdge(adj, 1, 2);
     addEdge(adj, 1, 3);
     addEdge(adj, 0, 4);
 
     Solution obj;
-    vector <int> ans = obj.bfsOfGraph(5, adj);
-    printAns(ans);
+
+    vector <int> ans = obj.bsfOfGraph(5, adj);
+    PrintBFS(ans);
 
     return 0;
 }
