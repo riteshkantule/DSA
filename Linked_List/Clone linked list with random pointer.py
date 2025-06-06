bc58ef6aeb3e3de52cbdc554faf201946d3041f5
Ritesh <106819295+riteshkantule@users.noreply.github.com>
2025-06-06 12:17:53 +0530

    Update with optimal solution
    TC = O(3N)
    SC = O(N)

--- Linked_List/Clone linked list with random pointer.py
+++ Linked_List/Clone linked list with random pointer.py
@@ -33,5 +33,54 @@ class Solution:
 
 # Best approach 
 
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+
+class Solution:
+    def copyRandomList(self, head: "Optional[Node]") -> "Optional[Node]":
+        
+        if not head:
+            return None
+        
+        temp = head
+
+        while temp:
+            new_Node = Node(temp.val)
+            new_Node.next = temp.next
+            temp.next = new_Node
+
+            temp = temp.next.next
+        
+        temp = head
+
+        while temp:
+            new_Node = temp.next
+            if temp.random is not None:
+                new_Node.random = temp.random.next
+            else:
+                new_Node.random = None
+            temp = temp.next.next
+
+        curr = head
+
+        dummy = Node(-1)
+        dummy.next = curr.next
+        res = dummy
+
+        while curr:
+            res.next = curr.next
+            curr.next = curr.next.next
+            
+            curr = curr.next
+            res = res.next
+        
+        return dummy.next
 
 
